eb37d6a7702edafa09e3b5c56bfca7e9c57a9589
chenwenzhuo <Chenwz1996@163.com>
2019-09-23 17:07:50 +0800

    删除了无用的import语句

--- SpringDemo/src/main/java/com/springmvcdemo/config/ViewConfig.java
+++ SpringDemo/src/main/java/com/springmvcdemo/config/ViewConfig.java
@@ -1,7 +1,5 @@
 package com.springmvcdemo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
